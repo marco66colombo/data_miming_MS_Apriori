44fe30c2c068342a293fb8381520986546720e51
marco66colombo <80001772+marco66colombo@users.noreply.github.com>
2022-09-02 11:13:43 -0500

    input parsing

--- main.py
+++ main.py
@@ -2,6 +2,8 @@
 from config.config import ROOT_DIR
 
 transactions = []
+SDC = 0.0
+MIS = {}
 
 with open(ROOT_DIR + '/files/input_data.txt', 'r') as filestream:
     for line in filestream:
@@ -9,10 +11,44 @@ with open(ROOT_DIR + '/files/input_data.txt', 'r') as filestream:
         currentline[-1] = currentline[-1].strip()
         transactions.append(currentline)
 
-f = open(ROOT_DIR + '/files/input_parameters.txt','r')
+with open(ROOT_DIR + '/files/input_parameters.txt', 'r') as filestream:
 
+    for line in filestream:
 
+        if line.startswith('MIS'):
+            first = line.split('(')
+            first[-1] = first[-1].strip()
+            second = first[1].split(")")
+            third = second[1].split("=")
+
+            if second[0] == "rest":
+                MIS['rest'] = float(third[1])
+            else:
+                MIS[second[0]] = float(third[1])
+
+        if line.startswith('SDC'):
+            currentline = line.split('=')
+            SDC = float(currentline[1])
+
+    print(transactions)
+    print(SDC)
+    print(MIS)
+
+def sort(transaction, sorting):
+    transaction = sorted(transaction, sorting)
+def init(transactions):
+    for transaction in transactions:
+        sort(transaction, lambda i : {MIS[i] if MIS.get(i) is not None else MIS['rest']})
+
+init(transactions)
 print(transactions)
+
+
+
+
+
+
+
 
 
 #dizionario per contare le occorrenze e poi ciclo sui valori per eliminare quelli sotto al minsup
